427c893a6299f9e23321d6b55e6cab215532680f
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-09-15 15:49:12 +0000

    se agregan nuevos datos a las tablas

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -15,7 +16,7 @@ class Usuario(Base):
     lastname = Column(String(250), nullable=False)
     email = Column(String(20), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
-    SuscriptionDate = Column(String(50), nullable=False)
+    created_date = Column(DateTime, default=datetime.datetime.now)
 
     favoritos = relationship('Favoritos', back_populates='usuario') 
 
@@ -37,6 +38,11 @@ class Personajes(Base):
     __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
     name = Column(String(30), nullable=False)
+    especie = Column(String(50)) 
+    altura = Column(Float)        
+    peso = Column(Float)          
+    pelicula_origen = Column(String(100))
+    description = Column(String(250))
 
     favoritos = relationship('Favoritos', back_populates='personajes')
 
@@ -44,6 +50,12 @@ class Planetas(Base):
     __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
     name = Column(String(30), nullable=False)
+    climate = Column(String(50))
+    population = Column(String(50))
+    diametro = Column(Float)
+    gravedad = Column(String(50))
+    terreno = Column(String(50))
+    pelicula_origen = Column(String(100))
 
     favoritos = relationship('Favoritos', back_populates='planetas')
 
@@ -51,6 +63,11 @@ class Vehiculos(Base):
     __tablename__ = 'vehiculos'
     id = Column(Integer, primary_key=True)
     name = Column(String(30), nullable=False)
+    modelo = Column(String(50))
+    fabricante = Column(String(50))
+    longitud = Column(Float)
+    velocidad_maxima = Column(Float)
+    capacidad_pasajeros = Column(Integer)
 
     favoritos = relationship('Favoritos', back_populates='vehiculos')
    
